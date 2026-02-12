e221c12a8a323969ddb51c075273e21a8de2cf46
blackingg <odetundemubarak04@gmail.com>
2026-02-12 03:57:06 +0100

    refactor: enhance styling and accessibility for Checkbox, Divider, and SocialLoginButton components

--- app/components/Form/Divider.tsx
+++ app/components/Form/Divider.tsx
@@ -4,10 +4,12 @@ export const Divider: React.FC<{ text?: string }> = ({
   <div className="my-8">
     <div className="relative">
       <div className="absolute inset-0 flex items-center">
-        <div className="w-full border-t border-gray-300" />
+        <div className="w-full border-t border-gray-100 dark:border-neutral-800" />
       </div>
-      <div className="relative flex justify-center text-sm">
-        <span className="px-2 bg-white text-gray-500">{text}</span>
+      <div className="relative flex justify-center text-xs uppercase tracking-wider font-semibold">
+        <span className="px-4 bg-white dark:bg-neutral-900 text-gray-400 dark:text-neutral-500">
+          {text}
+        </span>
       </div>
     </div>
   </div>
