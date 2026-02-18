576cc90bd28533d3c3e1f38938c2b35c05ffb011
David Atem <atembarry12@gmail.com>
2026-02-18 08:50:07 +0100

    Minor: Updated layout.tsx file on /upload-and-read

--- app/app/upload-and-read/layout.tsx
+++ app/app/upload-and-read/layout.tsx
@@ -1,20 +1,11 @@
-import { PageHeader } from "@/app/components/PageHeader"
-import { Sidebar } from "@/app/components/Sidebar"
-
 export default function ViewerLayout({
-    children
-} : {
-    children: React.ReactNode
-}){
-
-    return(
-        <div className="flex flex-col md:flex-row h-screen bg-gray-50 dark:bg-neutral-900 overflow-hidden">
-            <Sidebar/>
-            <main className="flex-1 flex flex-col min-w-0 overflow-hidden">
-                <PageHeader/>
-                  <div className='flex-1 overflow-y-auto custom-scrollbar'>{children}</div>
-            </main>
-          
-        </div>
-    )
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col md:flex-row h-screen bg-gray-50 dark:bg-neutral-900 overflow-hidden">
+      <div className="flex-1 overflow-y-auto custom-scrollbar">{children}</div>
+    </div>
+  );
 }
