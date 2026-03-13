81f78f3e03d043df93bdbd84c956d84da1e764ee
blackingg <odetundemubarak04@gmail.com>
2026-03-13 01:25:11 +0100

    fix: update department navigation to use slug instead of id

--- app/app/library/departments/page.tsx
+++ app/app/library/departments/page.tsx
@@ -78,7 +78,7 @@ export default function DepartmentsPage() {
                         department={department}
                         onClick={() =>
                           router.push(
-                            `/app/library/departments/${department.id}`,
+                            `/app/library/departments/${department.slug}`,
                           )
                         }
                       />
