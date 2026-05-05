69383430c2f8c4cd6ec4711512270963581e654c
blackingg <odetundemubarak04@gmail.com>
2026-05-05 22:14:28 +0100

    refactor(hooks): extract useDebounce to shared hooks and unify usage

--- app/hooks/index.ts
+++ app/hooks/index.ts
@@ -1,5 +1,6 @@
 export * from "./useFolderPermissions";
 export * from "./useIsOwner";
 export * from "./usePersistentReaderSettings";
+export * from "./useDebounce";
 export * from "./useResponsiveLimit";
 export * from "./useUpload";
