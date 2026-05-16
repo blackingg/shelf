25845799a934b90e770dc5acc403db5181cc2386
blackingg <odetundemubarak04@gmail.com>
2026-05-16 16:24:17 +0100

    #

--- app/components/Layout/AppHeader.tsx
+++ app/components/Layout/AppHeader.tsx
@@ -21,7 +21,7 @@ export const AppHeader: React.FC<{
 
   return (
     <nav
-      className={`bg-white/80 dark:bg-black/80 backdrop-blur-md sticky top-0 z-50 px-6 transition-all duration-200 ${
+      className={`bg-white/80 dark:bg-black/80 backdrop-blur-md sticky top-0 z-50 px-4 md:px-6 transition-all duration-200 ${
         isScrolled
           ? "border-b border-gray-100 dark:border-white/10 shadow-sm"
           : "border-b border-transparent"
@@ -30,7 +30,7 @@ export const AppHeader: React.FC<{
       <div className="max-w-7xl mx-auto">
         <div className="flex justify-between items-center h-16">
           <Link href={"/"} className="flex items-center gap-2">
-            <LogoStacked className="w-28 h-8 text-primary" />
+            <LogoStacked className="w-24 h-7 md:w-28 md:h-8 text-primary" />
           </Link>
           {rightContent}
         </div>
