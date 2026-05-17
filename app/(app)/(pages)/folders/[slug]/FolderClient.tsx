5b494bd53cd47aff32341a9d704a7273c1768462
blackingg <odetundemubarak04@gmail.com>
2026-05-17 09:56:20 +0100

    fix(FolderCard): update childrenCount calculation to handle empty children array

--- app/(app)/(pages)/folders/[slug]/FolderClient.tsx
+++ app/(app)/(pages)/folders/[slug]/FolderClient.tsx
@@ -294,7 +294,7 @@ export default function FolderClient() {
                   <FolderIcon
                     visibility={folder.visibility}
                     booksCount={folder.booksCount}
-                    childrenCount={folder.childrenCount}
+                    childrenCount={children && children.length > 0 ? children.length : folder.childrenCount || 0}
                     className="w-20 h-20 md:w-24 md:h-24 rounded-sm overflow-hidden"
                     width="100%"
                     height="100%"
